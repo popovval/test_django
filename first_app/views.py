c3d567035503d857da3f6c7847bee52fab9e08b2
PopovVI <popovvi@sibur.ru>
2022-03-30 11:52:53 +0300

    add views.py

--- first_app/views.py
+++ first_app/views.py
@@ -11,15 +11,22 @@ def get_info_from_close_mr(data):
                 'mr_id': data.get('object_attributes').get('iid'),
                 'action': data.get('object_attributes').get('action')}
 
+#TODO
+# 1) Secret token
+# 2) Проверка pr_id, mr_id, action на None
+# 3) .get('object_attributes').get('action') - переделать
+# 4) data.get('object_attributes') - в переменную
+# 5) стандартная функция (см применимость)
+# 6) Оставить ответ в виде json
 
 def post_list(request):
-    r = request.read()
-    data = json.loads(r)
-    resp = get_info_from_close_mr(data)
-    if resp is not None:
-        Mrs.objects.create(project_id=int(resp.get('pr_id')), mr_id=int(resp.get('mr_id')))
-        response = 'MR will be added to database'
-    else:
-        response = 'This MR is not closed'
+    # r = request.read()
+    # data = json.loads(r)
+    # resp = get_info_from_close_mr(data)
+    # if resp is not None:
+    #     Mrs.objects.create(project_id=int(resp.get('pr_id')), mr_id=int(resp.get('mr_id')))
+    #     response = 'MR will be added to database'
+    # else:
+    #     response = 'This MR is not closed'
 
-    return render(request, 'post_list.html', {'response': response})
+    return render(request, 'post_list.html', {'response': request.headers.get('X-Gitlab-Token')})
